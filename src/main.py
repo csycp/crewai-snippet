2fc20962d7daa0e758509051edfdf4d355a95eb9
csycp <frontend@acalab.co.kr>
2024-06-02 19:01:43 +0900

    테크스 하나 해보기

--- src/main.py
+++ src/main.py
@@ -1,2 +1,37 @@
+from dotenv import load_dotenv
+import os
+from crewai import Agent, Task, Crew
+
+load_dotenv()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+
 def main():
-    print("hi")
+    joker = Agent(
+        role="joke generator",
+        goal="generate very funny joke",
+        backstory=(
+            "The world is becoming darker and people are losing their laughter. It's time we need some funny jokes for adults to brighten things up again."
+        ),
+    )
+
+    generate_joke = Task(
+        description=(
+            "Make up a very very naughty and funny joke in Korean. It should be in the form of a dialogue."
+        ),
+        expected_output="dialogue-style joke.",
+        agent=joker,
+    )
+
+    crew = Crew(
+        agents=[joker],
+        tasks=[generate_joke],
+    )
+
+    result = crew.kickoff()
+
+    print(result)
