5b325d6e0abf951d185a8cda8410984a73894bf0
csycp <frontend@acalab.co.kr>
2024-06-02 21:07:32 +0900

    툴 사용해보기

--- src/main.py
+++ src/main.py
@@ -1,35 +1,41 @@
 from dotenv import load_dotenv
 import os
 from crewai import Agent, Task, Crew
+from crewai_tools import SerperDevTool
 
 load_dotenv()
 
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
 
+os.environ["SERPER_API_KEY"] = SERPER_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
+search_tool = SerperDevTool()
+
+
 def main():
-    joker = Agent(
-        role="joke generator",
-        goal="generate very funny joke",
+    researcher = Agent(
+        role="AI news researcher",
+        goal="Research and understand new information about the AI industry, which is introducing various technologies.",
         backstory=(
-            "The world is becoming darker and people are losing their laughter. It's time we need some funny jokes for adults to brighten things up again."
+            "People are currently confused by the rapid development of AI technology, particularly LLM technology. Some see AI as an opportunity, while others are skeptical, viewing it as a technology that could destroy the world. In order for humans not to be dominated by technology but to use it as a tool, you need to understand and provide AI-related articles and news to help people understand AI."
         ),
+        tools=[search_tool],
     )
 
-    generate_joke = Task(
-        description=(
-            "Make up a very very naughty and funny joke in Korean. It should be in the form of a dialogue."
-        ),
-        expected_output="dialogue-style joke.",
-        agent=joker,
+    research_about_AI = Task(
+        description="research the top five most influential AI-related news topics in Korea as of today.",
+        expected_output="A comprehensive list of 5 news on the latest AI trends.",
+        agent=researcher,
+        tools=[search_tool],
     )
 
     crew = Crew(
-        agents=[joker],
-        tasks=[generate_joke],
+        agents=[researcher],
+        tasks=[research_about_AI],
     )
 
     result = crew.kickoff()
